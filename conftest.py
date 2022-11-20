e1beec5246565489580dfbadf1bcf5569b27e886
Veronika Kopena <vkopena@gmail.com>
2022-11-20 23:38:41 +0300

    Sprint#4_1

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import pytest
 from selenium import webdriver
+
 @pytest.fixture()
 def driver():
     driver = webdriver.Firefox(executable_path="/Users/veronikakopena/Desktop/qa_python_tasks-main/geckodriver")
     yield driver
     driver.quit()
+
+
--- tests /test_main_page.py
+++ tests /test_main_page.py
@@ -1,24 +1,17 @@
-from selenium import webdriver
 from pages.main_page import MainPage
+import allure
 
 class TestMainPage:
+    @allure.title('Проверка переходов на сайте')
+    def test_move_to_yandex_page(self, driver):
+        main_page = MainPage(driver)
+        driver.get("https://qa-scooter.praktikum-services.ru/")
+        main_page.click_logo_yandex(driver).click()
+        assert len(driver.window_handles) == 2
 
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Firefox(executable_path="/Users/veronikakopena/Desktop/qa_python_tasks-main/geckodriver")
+    def test_move_to_samokat_page(self, driver):
+        main_page = MainPage(driver)
+        driver.get("https://qa-scooter.praktikum-services.ru/order")
+        main_page.click_logo_samokat(driver).click()
+        assert driver.current_url == "https://qa-scooter.praktikum-services.ru/"
 
-    def test_move_to_yandex_page(self):
-        main_page = MainPage(self.driver)
-        self.driver.get("https://qa-scooter.praktikum-services.ru/")
-        main_page.click_logo_yandex().click()
-        assert len(self.driver.window_handles) == 2
-
-    def test_move_to_samokat_page(self):
-        main_page = MainPage(self.driver)
-        self.driver.get("https://qa-scooter.praktikum-services.ru/order")
-        main_page.click_logo_samokat().click()
-        assert self.driver.current_url == "https://qa-scooter.praktikum-services.ru/"
-
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
