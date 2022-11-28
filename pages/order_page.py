57e1ef08725c7abf394c5f23d50897f2c6bfa4f0
Veronika Kopena <vkopena@gmail.com>
2022-11-28 22:33:48 +0300

    Sprint_4(1.3)

--- pages/order_page.py
+++ pages/order_page.py
@@ -9,11 +9,23 @@ class OrderPage:
     datepicker_locator = [By.XPATH, "//*[contains(@class, 'react-datepicker__month-container')]"]
     status_of_order = [By.XPATH, "//*[contains(@class, 'Order_Modal')]"]
     check_button_down = [By.XPATH, "//*[contains(@class, 'Button_Middle')]"]
+    field_station_locator = [By.XPATH, "//input[@placeholder='* Станция метро']"]
+    station1_locator = [By.XPATH, ".//*[@data-index='1']"]  # Станция 1
+    list_of_stations = [By.XPATH, "//*[contains(@class, 'select-search')]"]
+    input_name_locator = [By.XPATH, "//input[@placeholder='* Имя']"]
+    wait_when_samokat = [By.XPATH, "//input[@placeholder='* Когда привезти самокат']"]
     def button_order_up_locator(self):
         return self.driver.find_element(*OrderLocators.button_order_up_locator)
     def wait_button_order_down(self):
-        WebDriverWait(self.driver, 60).until(expected_conditions.element_to_be_clickable(self.check_button_down))
-
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.check_button_down))
+    def wait_select_station_locator(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.field_station_locator))
+    def wait_select_station_1(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.station1_locator))
+    def wait_name_locator(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.input_name_locator))
+    def wait_when_samokat(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.wait_when_samokat))
     def button_order_down(self):
         return self.driver.find_element(*OrderLocators.button_order_down_locator)
     def button_cookie_locator(self):
@@ -82,11 +94,21 @@ class OrderPage:
         self.comment_registration_locator().send_keys(comment)
 
     def fill_register_fields(self, name, surname, address, phone):
-        self.set_name(name)
-        self.set_surname(surname)
-        self.set_address(address)
-        self.set_phone(phone)
+        return self.set_name(name), self.set_surname(surname), self.set_address(address), self.set_phone(phone)
+    def click_button_up(self):
+        return self.button_cookie_locator().click(), self.button_order_up_locator().click()
+    def select_station_1(self):
+        return self.select_station_locator().click(), self.select_list_of_stations_locator(), self.set_station_one_locator().click()
+    def click_process_order_page_2(self):
+        return self.set_date_locator().click(), self.wait_for_load_datepicker_locator(), self.select_datepicker(), self.set_current_date().click(), self.set_duration_locator().click(), self.set_list_of_durations(), self.set_current_duration().click(),  self.set_colour_black_locator().click()
+    def click_confirm_buttons(self):
+        return self.click_button_confirm_locator().click(), self.click_answer_yes_locator().click(), self.check_popup_status()
 
-
-
-
+    def click_button_down(self):
+        return self.button_cookie_locator().click(), self.wait_button_order_down(), self.button_order_down().click()
+    def select_station_2(self):
+        return self.select_station_locator().click(), self.select_list_of_stations_locator(), self.set_station_two_locator().click()
+    def click_process_order_page_set_2(self):
+        return self.set_date_locator().click(), self.wait_for_load_datepicker_locator(), self.select_datepicker(), self.set_current_date2().click(), self.set_duration_locator().click(), self.set_list_of_durations(), self.set_current_duration2().click(), self.set_colour_grey_locator().click()
+    def click_confirm_buttons_2(self):
+        return self.click_button_confirm_locator().click(), self.click_answer_yes_locator().click(), self.click_popup_status_button().click()
